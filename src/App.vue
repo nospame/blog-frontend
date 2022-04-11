ef50dc290ae16ff7255954436ac21ee916822265
nospame <mapson.evan@gmail.com>
2022-04-11 14:02:07 -0700

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -8,32 +8,10 @@
     <router-link to="/login">Log In</router-link>&nbsp;|
     <router-link to="/logout">Log Out</router-link>
   </nav>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #990099;
-}
-
-img {
-  max-width: 85%;
-}
 </style>
